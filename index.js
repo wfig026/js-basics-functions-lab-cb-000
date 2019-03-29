94de81139f6b9fe3c82e35376645e985fe079fda
Will Figueiredo <will@wfigueiredo.com>
2019-03-29 01:52:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function calculatesFarePrice(start, destination) {
   } else if (distance > 2000 && distance < 2500) {
     return 25;
   } else {
-    return 'cannot travel that far'
+    return 'cannot travel that far';
   }
 };
